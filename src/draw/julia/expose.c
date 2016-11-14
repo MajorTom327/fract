380eb6c4cba8a5cbc3804ae4d4892f4350bef958
majortom327 <vthomas@student.42.fr>
2016-11-14 17:34:54 +0100

    Clear debug info

--- src/draw/julia/expose.c
+++ src/draw/julia/expose.c
@@ -6,7 +6,7 @@
 /*   By: vthomas <vthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 16:27:25 by vthomas           #+#    #+#             */
-/*   Updated: 2016/11/14 15:17:03 by vthomas          ###   ########.fr       */
+/*   Updated: 2016/11/14 17:33:58 by vthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,9 @@ int	exp_julia(void *param)
 	t_data	*d;
 
 	d = (t_data *)param;
-	ft_putendl("Calculate started...");
-	ft_putnbr_desc("WIDTH:\t", W_WID);
-	ft_putnbr_desc("HEIGHT:\t", W_HEI);
+//	ft_putendl("Calculate started...");
 	juliacalculate(d);
-	ft_putendl("Calculate finished !");
+//	ft_putendl("Calculate finished !");
 	mlx_put_image_to_window(d->mlx, d->win, d->img->img, 0, 0);
 	return (0);
 }
--- src/event.c
+++ src/event.c
@@ -6,7 +6,7 @@
 /*   By: vthomas <vthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 16:09:42 by vthomas           #+#    #+#             */
-/*   Updated: 2016/11/14 17:31:06 by vthomas          ###   ########.fr       */
+/*   Updated: 2016/11/14 17:34:09 by vthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ int		mouse_motion(int x, int y, void *p)
 		return (0);
 	if (x < 0 || x > W_WID || y < 0 || y > W_HEI)
 		return (0);
-	ft_putendl("MOTION EVENT !!");
 	f->c_r = x * 2 / W_WID - 1;
 	f->c_i = y * 2 / W_HEI - 1;
 	exp_julia(p);
